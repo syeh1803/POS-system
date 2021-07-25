0043a56dc345e6b59cff8e5e93b8f693afa54b96
syeh1803 <shaniyeh0318@gmail.com>
2021-07-25 16:32:53 +0800

    Use Drink Constructor to create drink instance

--- main.js
+++ main.js
@@ -1,23 +1,6 @@
 // new AlphaPos instance
 const alphaPos = new AlphaPos();
 
-const addDrinkButton = document.querySelector('[data-alpha-pos="add-drink"]');
-addDrinkButton.addEventListener("click", function () {
-  // 1. 取得選擇的飲料名稱, 冰塊, 甜度
-  // 利用input裡的checked是否為true，來檢查店員是否選到該品項
-  const drinkName = alphaPos.getCheckedValue('drink')
-  const ice = alphaPos.getCheckedValue('ice')
-  const sugar = alphaPos.getCheckedValue('sugar')
-  console.log(`${drinkName}, ${ice}, ${sugar}`)
-  // 2. 如果沒有選擇飲料的話，跳出提示視窗
-  if(!drinkName){
-    alert(`Please select at least one item.`)
-    return
-  }
-  // 3. 建立飲料instance, 並取得飲料價格
-  // 4. 將飲料實例產生成左側訂單區的畫面
-});
-
 // AlphaPos Constructor function - 放所有點餐機的功能
 function AlphaPos() {}
 
@@ -57,3 +40,25 @@ Drink.prototype.price = function () {
       alert("This drink does not exist.");
   }
 };
+
+const addDrinkButton = document.querySelector('[data-alpha-pos="add-drink"]');
+addDrinkButton.addEventListener("click", function () {
+  // 1. 取得選擇的飲料名稱, 冰塊, 甜度
+  // 利用input裡的checked是否為true，來檢查店員是否選到該品項
+  const drinkName = alphaPos.getCheckedValue('drink')
+  const ice = alphaPos.getCheckedValue('ice')
+  const sugar = alphaPos.getCheckedValue('sugar')
+  console.log(`${drinkName}, ${ice}, ${sugar}`)
+  // 2. 如果沒有選擇飲料的話，跳出提示視窗
+  if(!drinkName){
+    alert(`Please select at least one item.`)
+    return
+  }
+  // 3. 建立飲料instance, 並取得飲料價格
+  const drink = new Drink(drinkName, ice, sugar)
+  console.log(drink)
+  console.log(drink.price())
+  // 4. 將飲料實例產生成左側訂單區的畫面
+});
+
+
