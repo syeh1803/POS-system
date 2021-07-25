bee28621f08f4af5190f6638ea2c3bb16c42302c
syeh1803 <shaniyeh0318@gmail.com>
2021-07-25 16:19:04 +0800

    Code refactor

--- main.js
+++ main.js
@@ -1,37 +1,38 @@
-// Constructor function for Drinks
+// new AlphaPos instance
+const alphaPos = new AlphaPos();
 
+const addDrinkButton = document.querySelector('[data-alpha-pos="add-drink"]');
+addDrinkButton.addEventListener("click", function () {
+  // 1. 取得選擇的飲料名稱, 冰塊, 甜度
+  // 利用input裡的checked是否為true，來檢查店員是否選到該品項
+  const drinkName = alphaPos.getCheckedValue('drink')
+  const ice = alphaPos.getCheckedValue('ice')
+  const sugar = alphaPos.getCheckedValue('sugar')
+  console.log(`${drinkName}, ${ice}, ${sugar}`)
+  // 2. 如果沒有選擇飲料的話，跳出提示視窗
+  // 3. 建立飲料instance, 並取得飲料價格
+  // 4. 將飲料實例產生成左側訂單區的畫面
+});
+
+// AlphaPos Constructor function - 放所有點餐機的功能
+function AlphaPos() {}
+
+AlphaPos.prototype.getCheckedValue = function (inputName) {
+  let selectedOption = "";
+  document.querySelectorAll(`[name=${inputName}]`).forEach((item) => {
+    if (item.checked) {
+      selectedOption = item.value;
+    }
+  });
+  return selectedOption;
+};
+
+// Constructor function for Drinks
 function Drink(name, sugar, ice) {
   this.name = name;
   this.sugar = sugar;
   this.ice = ice;
 }
-
-// AlphaPos Constructor function - 放所有點餐機的功能
-function AlphaPos() {}
-
-const addDrinkButton = document.querySelector('[data-alpha-pos="add-drink"]');
-addDrinkButton.addEventListener("click", function () {
-  // 1. 取得選擇的飲料名稱, 冰塊, 甜度
-  // 2. 如果沒有選擇飲料的話，跳出提示視窗
-  // 3. 建立飲料instance, 並取得飲料價格
-  // 4. 將飲料實例產生成左側訂單區的畫面
-
-  // 利用input裡的checked是否為true，來檢查店員是否選到該品項
-  let allDrinksOptions = document.querySelectorAll('input[name="drink"]');
-  allDrinksOptions.forEach((option) => {
-    console.log(`${option.value}: ${option.checked}`);
-  });
-
-  let allIceOptions = document.querySelectorAll('input[name="ice"]');
-  allIceOptions.forEach((option) => {
-    console.log(`${option.value}: ${option.checked}`);
-  });
-
-  let allSugarOptions = document.querySelectorAll('input[name="sugar"]');
-  allSugarOptions.forEach((option) => {
-    console.log(`${option.value}: ${option.checked}`);
-  });
-});
 
 // price methods: get price according to the drink
 
